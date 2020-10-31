67f57c0646d2ebde753c4516e34855dc6cbba05c
grudoglon <56890955+grudoglon@users.noreply.github.com>
2020-10-31 17:43:35 +0300

    3

--- Server/src/network/ServerSocket.java
+++ Server/src/network/ServerSocket.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.*;
 import java.nio.ByteBuffer;
+import java.util.concurrent.*;
 
 @Slf4j
 public class ServerSocket {
@@ -14,10 +15,12 @@ public class ServerSocket {
     public static final int SOCKET_TIMEOUT = 3000;
 
     protected DatagramSocket socket;
+    protected ExecutorService executorService;
 
     public ServerSocket(InetSocketAddress a) throws SocketException {
         socket = new DatagramSocket(a);
         socket.setSoTimeout(SOCKET_TIMEOUT);
+        this.executorService = Executors.newFixedThreadPool(16);
     }
 
     public void sendDatagram(byte[] content, SocketAddress client) throws IOException {
@@ -40,17 +43,18 @@ public class ServerSocket {
     }
 
     public void sendResponse(Object response, SocketAddress client) {
-        try(ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectStream = new ObjectOutputStream(byteArrayStream)) {
-            objectStream.writeObject(response);
-            System.out.println("send object " + response.toString());
-            System.out.println("send object " + response.toString());
+        Future<Object> resulted = (Future<Object>) executorService.submit(() -> {
+            try (ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
+                 ObjectOutputStream objectStream = new ObjectOutputStream(byteArrayStream)) {
+                objectStream.writeObject(response);
+                System.out.println("send object " + response.toString());
+                System.out.println("send object " + response.toString());
 
-            sendDatagram(byteArrayStream.toByteArray(), client);
-
-        } catch (IOException e) {
-            System.out.println("Problem sending the response");
-        }
+                sendDatagram(byteArrayStream.toByteArray(), client);
+            } catch (IOException e) {
+                System.out.println("Problem sending the response");
+            }
+        });
     }
 
     public DatagramSocket getSocket() {
